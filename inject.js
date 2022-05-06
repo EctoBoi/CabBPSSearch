f319af44e1ef6d87889c0d6d98c4a150420e7d03
Justin <justin.banton@outlook.com>
2022-05-06 15:24:51 -0500

    Add files via upload

--- inject.js
+++ inject.js
@@ -19,11 +19,22 @@ function inject() {
         if (!document.getElementById('search-button')) {
             //Font
             let article = document.getElementsByClassName('product-article')[0]
+	    article.style.display= 'inline-flex'
+
+	    let bpssku = article.innerHTML.split('B: ')[1]
             if (article.innerHTML.split('B:')[1] === undefined) {
                 article.innerHTML = article.innerHTML.split('B:')[0] + ' B: N/A'
             } else {
-                article.innerHTML = article.innerHTML.split('B:')[0] + '<span style="font-size: 20px;font-weight: bold;">B:' + article.innerHTML.split('B:')[1] + '</span>'
+                article.innerHTML = article.innerHTML.split('B:')[0] + '<span style="font-size: 20px;font-weight: bold;margin-left: 4px;">B:' + bpssku + '</span>'
             }
+
+	    //Copy Button
+	    let copyDiv = document.createElement('div');
+            copyDiv.id = "copyButtonDiv"
+            copyDiv.style.marginLeft = "10px"
+
+            document.getElementsByClassName('product-article')[0].appendChild(copyDiv);
+	    copyButtonDiv.innerHTML = `<button id="copySKUButton" value="${bpssku}" onclick="navigator.clipboard.writeText(this.value)">Copy SKU</button>`
 
             //Search
             let button = document.createElement('button')
